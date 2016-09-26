9be97272137ea60a0fc66a985cec63e776be2ae0
Michael Beaver <michaelbeaver64@gmail.com>
2016-09-26 12:50:08 -0500

    Added functionality with a keys.json file to make individual projects unique.
    Removed excess code/files
    
    Added JSDoc comments to functions

--- controllers/encrpytionController.js
+++ controllers/encrpytionController.js
@@ -5,6 +5,12 @@ var encryption = require("./encryption");
 
 module.exports = {
   encryptionController: function(req, res) {
+    /**
+     * Function will take in request to encrypt password and respond with
+     *      encrypted password or respond with error message
+     * @param {express.req} req Request object with information sent by the user
+     * @param {express.res} res Response object to send result back to user
+     */
     var body = req.body;
     if(body.keyComponents == undefined || body.encryptionString == undefined) {
       res.send("incorrect components");
@@ -21,6 +27,12 @@ module.exports = {
   },
 
   decryptionController: function (req, res) {
+    /**
+     * Function will take in request to decrypt password and respond with
+     *      decrypted password or respond with error message
+     * @param {express.req} req Request object with information sent by the user
+     * @param {express.res} res Response object to send result back to user
+     */
     var body = req.body;
     if(body.keyComponents == undefined || body.encryptionString == undefined) {
       res.send("incorrect components");
@@ -35,11 +47,17 @@ module.exports = {
   },
 
   getHexKeys: function(req, res) {
+    /**
+     * Function to generate UUID4 hex keys, up to 2000 at a time
+     * @param {express.req} req Request object with information sent by the user
+     * @param {express.res} res Response object to send result back to user
+     */
+    var MAX_KEYS = 2000;
     var query = req.query;
     if (query.numKeys == undefined) {
       res.send("You need to specify numKeys");
     }
-    else if(query.numKeys < 300){
+    else if(query.numKeys <= MAX_KEYS){
       var keyList = [];
       var numKeys = query.numKeys;
 
--- controllers/encryption.js
+++ controllers/encryption.js
@@ -1,8 +1,16 @@
+"use strict";
 var CryptoJS = require('crypto-js');
+var keysJson = require('../keys.json');
 
 module.exports = {
 
   encryption: function (encryptionString, keyComponents) {
+    /**
+     * @param {string} encryptionString Password value to be encrypted
+     * @param {Array} keyComponents Array of key components to be used for
+     *      encryption
+     * @return {string} returns encrypted string
+     */
 
     var keyHash = getHash(keyComponents);
     var iv = keyHash.slice(0,16);
@@ -14,12 +22,17 @@ module.exports = {
   },
 
   decryption: function (encryptedString, keyComponents) {
+    /**
+     * @param {string} encryptedString Encrypted password value to be decrypted
+     * @param {Array} keyComponents Array of key components to be used for
+     *      decryption
+     * @return {string} returns decrypted string
+     */
 
     var keyHash = getHash(keyComponents);
     var iv = keyHash.slice(0,16);
 
     var decrypted = CryptoJS.AES.decrypt(encryptedString, keyHash, {iv: iv});
-    console.log(decrypted.toString());
 
     return decrypted.toString(CryptoJS.enc.Utf8);
   }
@@ -27,19 +40,51 @@ module.exports = {
 };
 
 var getHash = function(keyComponents){
-  var DEFAULT_KEY_1 = "c023d51f-8b8b-4025-9c77-268c41acad9c";
-  var DEFAULT_KEY_2 = "7f554a7c-b815-4c5b-95c4-e4dcac5cc627";
-
+  /**
+   * @param {Array} keyComponents The key components provided by the user
+   *
+   * @return {string} returns a hash to be used for encryption and decrpytion
+   */
   var sha512 = CryptoJS.algo.SHA512.create();
-  sha512.update(DEFAULT_KEY_1);
-  sha512.update(DEFAULT_KEY_2);
 
-  for(var keyComponent in keyComponents) {
-    sha512.update(keyComponent);
+  for(var key of keyComponents) {
+    sha512.update(key);
   }
+
+  var jsonKeys = getJsonKeys(keyComponents);
+  for(var jsonKey of jsonKeys){
+    sha512.update(jsonKey);
+  }
+
 
   var keyHash = sha512.finalize();
   keyHash = keyHash.toString(CryptoJS.enc.Base64);
-  // console.log("Hash: " + keyHash);
   return keyHash;
+};
+
+var getJsonKeys = function (keyComponents) {
+  /**
+   * This function selects the keys from the keys.json file that should be used
+   *       for encryption/decryption
+   * @param {Array} keyComponents This is a param that should be the
+   *      key components provided by the user
+   * @return {Array} returns Array of components selected from keys.json
+   */
+  var keyArray = [];
+  var MAX_NUM_KEYS = keysJson.keyComponents.length;
+  var EXPONENT = 3;
+
+  for(var key of keyComponents){
+    var jsonIndex = 1;
+    for(var char of key){
+      var num = parseInt(char);
+      if(!isNaN(num)){
+        jsonIndex += parseInt(char);
+      }
+    }
+    var index = Math.pow(jsonIndex, EXPONENT) % MAX_NUM_KEYS;
+    keyArray.push(keysJson.keyComponents[index]);
+  }
+
+  return keyArray;
 };
